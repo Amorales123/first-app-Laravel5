bfe673d9b7c2cdbf48fd3bfdda68c7e831e309fe
Angel Morales <angel.mcastellanos@gmail.com>
2015-05-16 20:42:57 -0600

    Agregando componentes al HtmlBuilder

--- app/Components/HtmlBuilder.php
+++ app/Components/HtmlBuilder.php
@@ -3,12 +3,67 @@
  * Extiendo la Clase de Laravel Collective para generar mis 
  * Propios Componentes como uno para generar Menú por ejemplo
  */
+
+// Utilizando iyección de dependencias
+use Illuminate\Contracts\Config\Repository as Config;
+use Illuminate\Contracts\View\Factory as View;
+//-- Fin de la inyección de Dependencias
+
 use Collective\Html\HtmlBuilder as CollectiveHtmlBuilder;
+use Illuminate\Routing\UrlGenerator;
 
 class HtmlBuilder extends CollectiveHtmlBuilder{
     
-    public function menu()
+    private $config;
+    private $view;    
+    
+    public function __construct(Config $config, View $view, UrlGenerator $url = null)
     {
-        return view('partials/menu');
+        $this->config = $config;
+        $this->view =   $view;
+        $this->url  =   $url;       
     }
+    
+    public function menu($items)
+    {
+        
+        if(! is_array($items)){
+            /*  
+             * Si No es array entonces paso una llave de la
+             * configuración de Laravel
+             */
+            $items = $this->config->get($items, array());
+        }
+        return $this->view->make('partials/menu', compact('items'));
+    }
+    
+    /**
+     * Builds an HTML class attribute dynamically
+     * Usage:
+     * {!! Html::classes(['home' => true, 'main', 'dont-use-this' => false]) !!}
+     * Returns:
+     *  class="home main".
+     *
+     * @param array $classes
+     *
+     * @return string
+     */
+    public function classes(array $classes)
+    {
+        $html = '';
+        foreach ($classes as $name => $bool) {
+            if (is_int($name)) {
+                $name = $bool;
+                $bool = true;
+            }
+            if ($bool) {
+                $html .= $name.' ';
+            }
+        }
+        if (! empty($html)) {
+            return ' class="'.trim($html).'"';
+        }
+        return '';
+    }
+    
 }
---
+++ config/teachme.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    
+    'menu'  => array(
+        'Tickets.latest'    => 'Recientes',
+        'Tickets.popular'   => 'Populares',
+        'Tickets.open'      => 'Abiertas',
+        'Tickets.closed'    => 'Finalizadas'
+    )
+);
